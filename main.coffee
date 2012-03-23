bbc2e8d2cd58c0bbb57e8abcdc24be48021b895b
Audrey Tang <audreyt@audreyt.org>
2012-03-23 18:56:13 +0800

    * Move main script to main.coffee.

---
+++ main.coffee
@@ -0,0 +1,23 @@
+solve = Solver(Dictionary)
+
+$ ->
+  $("#solve").submit ->
+    tiles = $("#letters").val()
+    tiles = tiles.toLowerCase().replace(/[^a-z]/g, "")
+    return  if tiles.length is 0
+    $("#letters").attr "disabled", true
+    $("#results").empty()
+    setTimeout (->
+      count = 0
+      solve tiles, Number($("#maxlen").val()) or 30, (hit) ->
+        count++
+        $("#results").append $("<tr />").append($("<th />").text(hit)).append($("<td />").text(hit.length))
+        count < 10
+
+      $("#letters").attr "disabled", false
+    ), 1
+    false
+
+  if /\w+/.test(location.hash)
+    $("#letters").val location.hash.toLowerCase().replace(/[^a-z]/g, "")
+    $("#solve").submit()
---
+++ main.js
@@ -0,0 +1,32 @@
+(function() {
+  var solve;
+
+  solve = Solver(Dictionary);
+
+  $(function() {
+    $("#solve").submit(function() {
+      var tiles;
+      tiles = $("#letters").val();
+      tiles = tiles.toLowerCase().replace(/[^a-z]/g, "");
+      if (tiles.length === 0) return;
+      $("#letters").attr("disabled", true);
+      $("#results").empty();
+      setTimeout((function() {
+        var count;
+        count = 0;
+        solve(tiles, Number($("#maxlen").val()) || 30, function(hit) {
+          count++;
+          $("#results").append($("<tr />").append($("<th />").text(hit)).append($("<td />").text(hit.length)));
+          return count < 10;
+        });
+        return $("#letters").attr("disabled", false);
+      }), 1);
+      return false;
+    });
+    if (/\w+/.test(location.hash)) {
+      $("#letters").val(location.hash.toLowerCase().replace(/[^a-z]/g, ""));
+      return $("#solve").submit();
+    }
+  });
+
+}).call(this);
